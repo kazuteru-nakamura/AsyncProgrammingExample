0c7ef51e9c6814fd6c21126c37e3ee414fd25589
kazuteru-nakamura <kazuteru.nakamura@molgroup.com>
2019-11-14 16:16:57 +0900

    First Commited

---
+++ Breakfast/Breakfast/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+
+namespace Breakfast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 朝食の準備を開始する
+            Console.WriteLine("Start!");
+
+            // コーヒーを注ぐ
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            // 卵を焼き始める
+            Console.WriteLine("egg fry start");
+            Egg eggs = FryEggs();
+            Console.WriteLine("eggs are ready");
+
+            // ベーコンを焼き始める
+            Console.WriteLine("bacon fry start");
+            Bacon bacon = FryBacon();
+            Console.WriteLine("bacon is ready");
+
+            // パンを焼き始める
+            Toast toast = ToastBread();
+
+            // 焼き終わったパンにバターとジャムを塗る
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            // オレンジジュースを注ぐ
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+
+            // 朝食できた！
+            Console.WriteLine("Breakfast is ready!");
+        }
+
+        private static Juice PourOJ()
+        {
+            Thread.Sleep(1000);
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast)
+        {
+            Thread.Sleep(1000);
+        }
+
+        private static void ApplyButter(Toast toast)
+        {
+            Thread.Sleep(1000);
+        }
+
+        private static Toast ToastBread()
+        {
+            Thread.Sleep(10000);
+            return new Toast();
+        }
+
+        private static Bacon FryBacon()
+        {
+            Thread.Sleep(5000);
+            return new Bacon();
+        }
+
+        private static Egg FryEggs()
+        {
+            Thread.Sleep(15000);
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Thread.Sleep(1000);
+            return new Coffee();
+        }
+    }
+
+    internal class Juice
+    {
+    }
+
+    internal class Toast
+    {
+    }
+
+    internal class Bacon
+    {
+    }
+
+    internal class Egg
+    {
+    }
+
+    internal class Coffee
+    {
+    }
+}
---
+++ BreakfastAsync/BreakfastAsync/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BreakfastAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 朝食の準備を開始する
+            Console.WriteLine("Start!");
+
+            // 卵を焼き始める
+            Console.WriteLine("egg fry start");
+            Task<Egg> eggTask = FryEggs();
+  
+            // ベーコンを焼き始める
+            Console.WriteLine("bacon fry start");
+            Task<Bacon> baconTask = FryBacon();
+
+            // パンを焼き始める
+            Console.WriteLine("toast start");
+            Task<Toast> toastTask = ToastBread();
+
+            // パンが焼き終わるのを待つ
+            //Toast toast = await toastTask;
+            Toast toast = toastTask.Result;
+
+            // 焼き終わったパンにバターとジャムを塗る
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            // オレンジジュースを注ぐ
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+
+            // コーヒーを注ぐ
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            // 卵が焼き終わるのを待つ
+            //Egg eggs = await eggTask;
+            Egg egg = eggTask.Result;
+            Console.WriteLine("eggs are ready");
+
+            // ベーコンが焼き終わるのを待つ
+            //Bacon bacon = await baconTask;
+            Bacon bacon = baconTask.Result;
+            Console.WriteLine("bacon is ready");
+
+            // 朝食できた！
+            Console.WriteLine("Breakfast is ready!");
+        }
+
+        private static Juice PourOJ()
+        {
+            System.Threading.Thread.Sleep(1000);
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast)
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private static void ApplyButter(Toast toast)
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private async static Task<Toast> ToastBread()
+        {
+            await Task.Delay(10000);
+            return new Toast();
+        }
+
+        private async static Task<Bacon> FryBacon()
+        {
+            await Task.Delay(5000);
+            return new Bacon();
+        }
+
+        private async static Task<Egg> FryEggs()
+        {
+            await Task.Delay(15000);
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            System.Threading.Thread.Sleep(1000);
+            return new Coffee();
+        }
+    }
+
+    internal class Juice
+    {
+    }
+
+    internal class Toast
+    {
+    }
+
+    internal class Bacon
+    {
+    }
+
+    internal class Egg
+    {
+    }
+
+    internal class Coffee
+    {
+    }
+}
